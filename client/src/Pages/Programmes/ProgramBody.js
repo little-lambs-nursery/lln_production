d2495acb7c7813ad8196ec9aa26d7a4ef3d9e1b4
little-lambs-nursery <119837678+little-lambs-nursery@users.noreply.github.com>
2022-12-28 12:43:21 +0530

    Update ProgramBody.js

--- client/src/Pages/Programmes/ProgramBody.js
+++ client/src/Pages/Programmes/ProgramBody.js
@@ -10,7 +10,7 @@ const ProgramBody = () => {
                             <div className='col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12'>
                                 <div className='programBodyContent pink'>
                                     <h4>blader & bowl</h4>
-                                    <p>Bladder and bowel training is also called as potty/toilet training. This is all about routine and consistency. Though potty training success depends on the developmental, behavioral and physical milestones of a child, children get ready between the ages of 16 months to 24 months showing signs of readiness (consult the teacher to know more about the signs of readiness). </p>
+                                    <p>Bladder and bowel training is also called potty/toilet training. This is all about routine and consistency. Though potty training success depends on the developmental, behavioural and physical milestones of a child, children get ready between the ages of 16 months to 24 months showing signs of readiness (consult the teacher to know more about the signs of readiness).</p>
                                 </div>
                                 <div className='programBodyContent blue'>
                                     <h4>Important message to parents</h4>
@@ -18,7 +18,7 @@ const ProgramBody = () => {
                                 <div className='row mb-5 mt-5'>
                                     <div className='col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12'>
                                         <div className='programBodyCard card1'>
-                                            <p>Do not equate the potty
+                                            <p>Do not equate potty
                                                 training success with
                                                 intelligence or
                                                 stubbornness.</p>
@@ -48,18 +48,18 @@ const ProgramBody = () => {
                                 </div>
                                 <div className='programBodyContent pink'>
                                     <h4>Child Anxiety Multi-day program</h4>
-                                    <p>Separation anxiety is seen in very young children from 18 months to 3 years. The signs of separation anxiety can be seen in an infant even as early as 4-5months. However, this is when it is considered as the baby being securely attached to the parent and it means that the baby has formed a strong bond with the parent. It is a normal part of growing up. Young children cry and are clingy when the caregivers/parents leave. Babies and toddlers grow out of this distress as they grow. However, there are ways to reduce the distress and CAMP is a graduated approach to deal with the signs/symptoms of separation anxiety</p>
+                                    <p>Separation anxiety is seen in very young children from 18 months to 3 years. The signs of separation anxiety can be seen in an infant even as early as 4-5months. However, this is when it is considered as the baby being securely attached to the parent and it means that the baby has formed a strong bond with the parent. It is a normal part of growing up. Young children cry and are clingy when the caregivers/parents leave. Babies and toddlers grow out of this distress as they grow. However, there are ways to reduce the distress and CAMP is a graduated approach to dealing with the signs/symptoms of separation anxiety</p>
                                 </div>
                                 <div className='programBodyContent blue'>
                                     <h4>Picky Eating Program</h4>
-                                    <p>Picky eating is just in simple terms battle over food. Sometimes it is just a response to controlling or pushy parents or to bribery. PEP will focus on supporting healthy eating habits by increasing the number of foods in the child’s repertoire. The final goal of the program is food acceptance; hence the entire program’s approach is based on positive reinforcement by all the key instructor of the program. To support the program parents are advised to consult the class teacher and hand over the below list-</p>
+                                    <p>Picky eating is just in simple terms a battle over food. Sometimes it is just a response to controlling or pushy parents or to bribery. PEP will focus on supporting healthy eating habits by increasing the number of foods in the child’s repertoire. The final goal of the program is food acceptance; hence the entire program’s approach is based on positive reinforcement by all the key instructors of the program. To support the program parents are advised to consult the class teacher and hand over the below list-</p>
                                 </div>
                                 <div className='programBodyCircleCardBox'>
                                     <ul>
                                         <li>
                                             <div className='programBodyCircleCard card1'>
                                                 <p>Foods that the
-                                                    child eat</p>
+                                                    child eats</p>
                                             </div>
                                         </li>
                                         <li>
@@ -79,15 +79,15 @@ const ProgramBody = () => {
                                 </div>
                                 <div className='programBodyContent pink'>
                                     <h4>Smart Juniorz Program</h4>
-                                    <p>This is a one-to-one teaching that takes place between a single child and one teacher. Rather than being in a group one-to-one teaching provides more interaction and fewer distractions. This program can be availed either after 3.30pm regularly or during camp breaks. The program is organized to focus more on pre-reading to reading stages; pre-writing to writing stages; pre-mathematics to mathematical concepts and phonic stages. This is a best program for our little ones who need a little more comfort, guidance, love and support or who are planning to take the next step of being in a formal school</p>
+                                    <p>This is a one-to-one teaching that takes place between a single child and one teacher. Rather than being in a group, one-to-one teaching provides more interaction and fewer distractions. This program can be availed either after 3.30pm regularly or during camp breaks. The program is organised to focus more on pre-reading to reading stages; pre-writing to writing stages; pre-mathematics to mathematical concepts and phonic stages. This is the best program for our little ones who need a little more comfort, guidance, love and support or who are planning to take the next step of being in a formal school</p>
                                 </div>
                                 <div className='programBodyContent blue'>
                                     <h4>IT/STEM program</h4>
-                                    <p>IT/STEM is for all the nursery children based on their age and development. However, children above 2 years will have a systematic curriculum based program for their success in the field of science, technology, engineering and mathematics. Our young minds will learn to make predictions and test the theories around the world. The program makes science more fun; children will learn the basic skills to use computers; engineering principles covered through Lego and building; mathematical skills through technology and fun.</p>
+                                    <p>IT/STEM is for all nursery children based on their age and development. However, children above 2 years will have a systematic curriculum-based program for their success in the field of science, technology, engineering and mathematics. Our young minds will learn to make predictions and test the theories around the world. The program makes science more fun; children will learn the basic skills to use computers; engineering principles covered through Lego and building; mathematical skills through technology and fun.</p>
                                 </div>
                                 <div className='programBodyContent pink'>
                                     <h4>Academic Enrichment Program</h4>
-                                    <p>AEP will help to break the barriers of curricular structures and allow children to create, design and learn without limits. It will help the child to discover new hobbies/interests outside the classroom setting. Below is the list of various AEP in LLN, however this is not the end as more and more will be added everytime-</p>
+                                    <p>AEP will help to break the barriers of curricular structures and allow children to create, design and learn without limits. It will help the child to discover new hobbies/interests outside the classroom setting. Below is the list of various AEPs in LLN, however, this is not the end as more and more will be added every time-</p>
                                 </div>
                                 <div className='porgramCategoryCardBox'>
                                     <ul>
@@ -132,7 +132,7 @@ const ProgramBody = () => {
                                 </div>
                                 <div className='programBodyContent blue'>
                                     <h4>Communication & Language Program</h4>
-                                    <p>Communication in preschoolers not only depends on the language but also on facial expressions, gestures and behaviors. Hence, there are three aspects in the CLP- </p>
+                                    <p>Communication in preschoolers not only depends on language but also on facial expressions, gestures and behaviours. Hence, there are three aspects in the CLP- </p>
                                 </div>
                                 <div className='progremHexagonShapeBox'>
                                     <ul>
@@ -195,7 +195,7 @@ const ProgramBody = () => {
                                 </div>
                                 <div className='programBodyContent blue'>
                                     <h4>About EYFS</h4>
-                                    <p>The British Early Years Foundation Stage Curriculum was introduced in England in September 2000. It is the first curriculum in England to cross the division between pre-school and school.</p>
+                                    <p>The British Early Years Foundation Stage Curriculum was introduced in England in September 2000. It is the first curriculum in England to cross the division between preschool and school.</p>
                                     <h5>The four guiding principles of the EYFS are:</h5>
                                     <div className='differenceBodyCard'>
                                         <ul>
@@ -221,13 +221,13 @@ const ProgramBody = () => {
                                     <h5>We have highlighted a few key features of this below for you to get a feel of what we do in the Little Lambs Nursery</h5>
                                     <div className='differenceBodyCard'>
                                         <ul>
-                                            <li>we recognise the fact that young children need to learn through practical, meaningful experiences rather than ‘rote’ learning.</li>
-                                            <li>we emphasise play as the fundamental vehicle of learning for a child.   </li>
-                                            <li>Personal, social, and emotional development. ... </li>
-                                            <li>Activities are designed from children’s interests and needs. </li>
+                                            <li>We recognise the fact that young children need to learn through practical, meaningful experiences rather than ‘rote’ learning.</li>
+                                            <li>We emphasise play as the fundamental vehicle of learning for a child.</li>
+                                            <li>Personal, social, and emotional development....</li>
+                                            <li>Activities are designed for children’s interests and needs.</li>
                                             <li>We include social studies and IT/STEM in our unique curriculum to get the children ready for their future.</li>
-                                            <li>we include daily variety of activies that enhances the child’s personal, social and emotional skills (as these activities will help to develop self confidence, perseverance and a willing attitude, to name but a few).</li>
-                                            <li>we promote the role of parents as we consider parents as equal partners in their child’s journey</li>
+                                            <li>We include a daily variety of activities that enhance the child’s personal, social and emotional skills (as these activities will help to develop self-confidence, perseverance and a willing attitude, to name but a few).</li>
+                                            <li>We promote the role of parents as we consider parents as equal partners in their child’s journey</li>
                                         </ul>
                                     </div>
                                 </div>
