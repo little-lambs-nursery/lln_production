2d98eb4248820d80146904ec28c5598b24aedc1f
little-lambs-nursery <119837678+little-lambs-nursery@users.noreply.github.com>
2022-12-28 12:50:18 +0530

    Update HealthSafetyBody.js

--- client/src/Pages/HealthSafetyContent/HealthSafetyBody.js
+++ client/src/Pages/HealthSafetyContent/HealthSafetyBody.js
@@ -10,20 +10,20 @@ const HealthSafetyBody = () => {
                             <div className="col-lg-8">
                                 <div className="healthSafetyBodyContent">
                                     <h2>Health & Safety</h2>
-                                    <p>In Little Lambs Nursery we believe that every child deserves the best start in life, hence we provide an environment to the children that is safe and secure. This is achieved with the help of comprehensive set of policies and procedures that meet the EYFS welfare requirements. Following procedures are in place to ensure health and safety of the children</p>
+                                    <p>At Little Lambs Nursery We Believe That Every Child Deserves The Best Start In Life, Hence We Provide An Environment To The Children That Is Safe And Secure. This Is Achieved With The Help Of a Comprehensive Set Of Policies And Procedures That Meet The EYFS Welfare Requirements. The following Procedures Are In Place To Ensure the Health And Safety Of The Children.</p>
                                     <div className='differenceBodyCard'>
                                     <ul>
-                                        <li>Daily risk assessment by an assigned staff to detect the risk/hazards and plan an action</li>
+                                        <li>A daily risk assessment by an assigned staff to detect the risk/hazards and plan an action</li>
                                         <li>Daily cleaning and sanitization of the nursery premises</li>
-                                        <li>Daily sterilization of the resources</li>
+                                        <li>Daily sterilisation of the resources</li>
                                         <li>Proper training of the staff who understand the handling of injuries, accidents and are well versed with the reporting procedure</li>
                                         <li>Termly emergency evacuation drills so that all present in the premises are aware of the evacuation procedure</li>
                                         <li>Timely maintenance of faulty equipment</li>
-                                        <li>All the sharp edges covered with protective liners/edge covers</li>
-                                        <li>Intercom system is in place in each classroom</li>
+                                        <li>All the sharp edges are covered with protective liners/edge covers</li>
+                                        <li>An intercom system is in place in each classroom</li>
                                         <li>All gates/doors are shut and there is continuous provision of CCTV</li>
                                         <li>Each teacher maintains the allergy list for children</li>
-                                        <li>Soft padded shock resistant flooring all over the nursery</li>
+                                        <li>Soft padded shock-resistant flooring all over the nursery</li>
                                         <li>Anti-slip mats placed wherever required</li>
                                         <li>Bright lighting to ensure children do not strain their eyes</li>
                                         <li>No trailing electrical cables/leads</li>
@@ -31,9 +31,9 @@ const HealthSafetyBody = () => {
                                         <li>Cleaning products are all kept in a safely locked cupboard away from the reach of the children</li>
                                         <li>Usage of equipment under adult supervision</li>
                                         <li>Children are supervised all the time</li>
-                                        <li>Fire- fighting equipment are in place</li>
-                                        <li>Full time DHA registered nurse</li>
-                                        <li>First aid trained staff</li>
+                                        <li>Fire-fighting equipment is in place</li>
+                                        <li>Full-time DHA registered nurse</li>
+                                        <li>First aid-trained staff</li>
                                     </ul>
                                     </div>
                                 </div>
