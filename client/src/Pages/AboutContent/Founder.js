80fcc5bfb2eb0f975bb4d26f0f28c3970232b043
little-lambs-nursery <119837678+little-lambs-nursery@users.noreply.github.com>
2022-12-28 12:31:04 +0530

    Update Founder.js

--- client/src/Pages/AboutContent/Founder.js
+++ client/src/Pages/AboutContent/Founder.js
@@ -9,19 +9,19 @@ const Founder = () => {
                                 <div className="founderContent">
                                     <h2>We, Dr. Yogita Maledan(CEO & FOUNDER) and Nishtha Righwani(FOUNDER)  of Little Lambs Nursery<br />
                                         take a pledge….</h2>
-                                    <p> At any time, children will always come first. With LLN, every day will be a new learning experience/opportunity for your child. Little Lambs Nursery pledges to provide a safe and secure home away from home. We would love to invite you to our Mankool branch to find out more about the way we work, and experience how we have embarked on a positive learning journey with our little ones and the support of our fabulous staff/parents.</p>
+                                    <p>At any time, children will always come first. With LLN, every day will be a new learning experience/opportunity for your child. Little Lambs Nursery pledges to provide a safe and secure home away from home. We would love to invite you to our Mankhool branch to find out more about the way we work and experience how we have embarked on a positive learning journey with our little ones and the support of our fabulous staff/parents.</p>
                                 </div>
                                 <div className="founderCard">
                                     <h4>Vision</h4>
-                                    <p>Promoting excellence and quality service in the field of education through clear policies and procedures to ensure customer satisfaction and create a culture of inclusion and empowerment to enhance organizational development.</p>
+                                    <p>Promoting excellence and quality service in the field of education through clear policies and procedures to ensure customer satisfaction and create a culture of inclusion and empowerment to enhance organisational development.</p>
                                 </div>
                                 <div className="founderCard">
                                     <h4>Mission</h4>
-                                    <p>To provide outstanding nurturing support and environment to children thereby enabling the development to achieve holistic growth with several opportunities to develop the child’s potential; achieving this through positive reinforcement; inspiring children to begin a learning journey with life-long enjoyment, the first step towards the best.</p>
+                                    <p>To provide outstanding nurturing support and environment to children, thereby enabling the development to achieve holistic growth with several opportunities to develop the child’s potential; achieving this through positive reinforcement; inspiring children to begin a learning journey with life-long enjoyment, the first step towards the best.</p>
                                 </div>
                                 <div className="founderCard">
                                     <h4>Values</h4>
-                                    <p>All members of Little Lambs Nursery will follow and apply the 6 core values that will build up the pillars of the organization, thereby applying into the curriculum:</p>
+                                    <p>All members of Little Lambs Nursery will follow and apply the 6 core values that will build up the pillars of the organisation, thereby applying to the curriculum:</p>
                                 </div>
                             </div>
                         </div>
