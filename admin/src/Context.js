c521779e6f57c6c73a4f0f6a617f59dd16fbced4
Himanshu <sharmagi1998@gmail.com>
2023-01-14 15:33:49 +0530

    admin login store in local storage

--- admin/src/Context.js
+++ admin/src/Context.js
@@ -1,18 +1,20 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
 
-const Admin=createContext()
+const Admin = createContext()
 
-const Context=({children})=>{
-    const [admin, setAdmin] = useState();
-
-    return(
-        <Admin.Provider value={{admin,setAdmin}}>
+const Context = ({ children }) => {
+    const [admin, setAdmin] = useState(JSON.parse(localStorage.getItem('adminInfo')) || null);
+    useEffect(() => {
+        localStorage.setItem('adminInfo', JSON.stringify(admin))
+    }, [admin])
+    return (
+        <Admin.Provider value={{ admin, setAdmin }}>
             {children}
         </Admin.Provider>
     )
 }
 
-export const AdminState=()=>{
+export const AdminState = () => {
     return useContext(Admin)
 }
 
