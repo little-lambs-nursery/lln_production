a8ae293dab92ee77e91cf361aa617f74ffbcc981
little-lambs-nursery <119837678+little-lambs-nursery@users.noreply.github.com>
2023-03-13 14:02:40 +0530

    Update Home.js
    Remove Organisation Schema Tag

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -17,30 +17,6 @@ const Home = (props) => {
                 <meta name="description" content="Little Lambs Nursery LLC is a well-recommended preschool & daycare center for early childhood learning and a secure environment in Mankhool, Dubai." />
                 <link rel="publisher" href="https://www.facebook.com/nurserylittlelambs" />
                 <link rel="canonical" href="https://www.nurserylittlelambs.com/" />
-        
-        <script type=""application/ld+json"">
-{
-  ""@context"" : ""http://schema.org"",
-  ""@type"" : ""Organization"",
-  ""name"" : ""Little Lambs Nursery"",
- ""url"" : ""https://www.nurserylittlelambs.com/"",
- ""logo"": ""https://www.nurserylittlelambs.com/static/media/logo.da83167e5538e10e6c23.webp"",
-  ""description"": ""Little Lambs Nursery LLC is a well-recommended preschool & daycare centre for early childhood learning and a secure environment in Mankhool, Dubai.""
- ""sameAs"" : [ ""https://www.linkedin.com/company/nursery-little-lambs/"",
-                ""https://www.facebook.com/nurserylittlelambs"",
-                ""https://twitter.com/nursery_lambs"",]
-  ""address"": {
-    ""@type"": ""PostalAddress"",
-    ""Street Address"": ""33rd Street. Near Mankhool Park/ ADCB Metro Statio"",
-    ""State"": ""Unite"",
-    ""Country"": ""United Arab Emirates""
-    ""Phone Number"": ""+971 524438743""
-
-  }
-}
-</script> 
-
-        
             </Helmet>
             <WhatsAppChat />
             <Navigation />
