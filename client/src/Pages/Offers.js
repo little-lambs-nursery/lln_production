522b209425c25624683d8f9b28f7761fbac74246
Websmart <websmartitsolutions.com@gmail.com>
2024-05-29 15:04:13 +0400

    Meta data changes-Offers

--- client/src/Pages/Offers.js
+++ client/src/Pages/Offers.js
@@ -8,8 +8,8 @@ const Offers = () => {
     return (
         <>
             <Helmet>
-                <title>Little Lambs Nursery | Offers </title>
-                <meta name="description" content="We have flexible plans along with the most suitable learning environment for your kids. Choose your plan and get amazing discounts!" />
+                <title> Affordable Nursery Offers | Little Lambs Nursery</title>
+                <meta name="description" content="We offer affordable nursery fees at Little Lambs in Dubai. Choose your plan and get amazing discounts Now!" />
                 <link rel="canonical" href="https://www.nurserylittlelambs.com/offers" />
             </Helmet>
             <Navigation />
