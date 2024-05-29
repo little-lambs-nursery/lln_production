b730390f5056dfdf151803a76586114decf20d47
Websmart <websmartitsolutions.com@gmail.com>
2024-05-29 15:02:35 +0400

    Meta data change-mProgrammes

--- client/src/Pages/Programmes.js
+++ client/src/Pages/Programmes.js
@@ -11,7 +11,7 @@ const Programmes = () => {
     return (
         <>
             <Helmet>
-                <title>British Curriculum Nursery | Little Lambs</title>
+                <title>Our Programmes | Little Lambs</title>
                 <meta name="description" content="Little Lambs Nursery LLC is a British Curriculum Nursery for childern in Mankhool, Dubai with multiple innovative programs. Contact now to learn more about us." />
                 <link rel="canonical" href="https://www.nurserylittlelambs.com/programmes" />
 
